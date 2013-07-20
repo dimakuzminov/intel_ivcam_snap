82694c5da668d120a1371f83a0fd65e3ac02aefb
Dmitry Kuzminov <dima@galilsoft.com>
2013-07-20 13:00:28 +0300

    1. Can switch between capture and preview mode 2. Add OpenGL view and test basic operation 3. Add calibration with double !!!!! Code for 3d is broken, due to handle only image. Currently Calibration is not working properly Code commited to make stage

--- ivcam.demo/src/com/example/ivcam/demo/CalibrationDoublePrecision.java
+++ ivcam.demo/src/com/example/ivcam/demo/CalibrationDoublePrecision.java
@@ -1,30 +1,30 @@
 package com.example.ivcam.demo;
 
-public class Calibration {
+public class CalibrationDoublePrecision {
 	public class Parameters {
-		public float[] kc = new float[9]; // Camera intrinsic [3x3]
-		public float[] distc = new float[5]; // Camera forward distortion [1x5]
-		public float[] invdistc = new float[5]; // Camera inverse distortion
+		public double[] kc = new double[9]; // Camera intrinsic [3x3]
+		public double[] distc = new double[5]; // Camera forward distortion [1x5]
+		public double[] invdistc = new double[5]; // Camera inverse distortion
 												// [1x5]
-		public float width = 0; // Camera width
-		public float height = 0; // Camera height
-		public float zmax = 200; // Maximum Z range. [0,65535] in the Z image is
+		public double width = 0; // Camera width
+		public double height = 0; // Camera height
+		public double zmax = 200; // Maximum Z range. [0,65535] in the Z image is
 									// mapped to [0,Zmax]
-		public float[] pp = new float[12];// ??? [3x4] : important [3x4]
+		public double[] pp = new double[12];// ??? [3x4] : important [3x4]
 											// required (can't be used with
 											// [2x4]
-		public float[] kp = new float[9]; // [3x3]
-		public float[] rp = new float[9]; // [3x3]
-		public float[] tp = new float[3];
-		public float[] distp = new float[5]; // [1x5]
-		public float[] invdistp = new float[5]; // [1x5]
-		public float[] pt = new float[12];// [3x4]
-		public float[] distt = new float[5]; // [1x5]
+		public double[] kp = new double[9]; // [3x3]
+		public double[] rp = new double[9]; // [3x3]
+		public double[] tp = new double[3];
+		public double[] distp = new double[5]; // [1x5]
+		public double[] invdistp = new double[5]; // [1x5]
+		public double[] pt = new double[12];// [3x4]
+		public double[] distt = new double[5]; // [1x5]
 	};
 
 	public class Point {
-		public float x;
-		public float y;
+		public double x;
+		public double y;
 
 		public Point(int x, int y) {
 			this.x = x;
@@ -38,7 +38,7 @@ public class Calibration {
 	public Point[] m_unprojCoeffs;
 	public int m_unprojAllocSize;
 
-	public Calibration(float[] paramData, int nParams) {
+	public CalibrationDoublePrecision(double[] paramData, int nParams) {
 		m_initialized = false;
 		m_unprojCoeffs = null;
 		m_unprojAllocSize = 0;
@@ -49,11 +49,11 @@ public class Calibration {
 		m_initialized = false;
 	}
 
-	public float getZMax() {
+	public double getZMax() {
 		return params.zmax;
 	};
 
-	public float[] getParameters() {
+	public double[] getParameters() {
 		if (m_initialized)
 			return params.kc;
 		else
@@ -64,15 +64,15 @@ public class Calibration {
 		return 82;
 	}
 
-	public Boolean buildParameters(float[] paramData, int nParams) {
-		float[] ps = new float[128];
+	public Boolean buildParameters(double[] paramData, int nParams) {
+		double[] ps = new double[128];
 		for (int i = 0; i < nParams; ++i) {
 			ps[i] = paramData[i];
 		}
 		return buildParametersAux(ps, nParams);
 	}
 
-	public Boolean buildParametersAux(float[] paramData, int nParams) {
+	public Boolean buildParametersAux(double[] paramData, int nParams) {
 		final int kParamsCount = 61;
 
 		if (nParams < kParamsCount)
@@ -128,7 +128,7 @@ public class Calibration {
 		if (sizeToAlloc == m_unprojAllocSize) {
 			Boolean sameParams = true;
 			int k = nParamters();
-			float[] params = getParameters();
+			double[] params = getParameters();
 			for (i = 0; i != k; ++i) {
 				if (params[i] != paramData[i]) {
 					sameParams = false;
@@ -149,28 +149,28 @@ public class Calibration {
 		return true;
 	}
 
-	public void buildUV(float x, float y, float z, float[] u, float[] v) {
+	public void buildUV(double x, double y, double z, double[] u, double[] v) {
 		// x, y, z: must be unprojected coordinates
 		// u, v are in the range [0, 1]
-		float[] pt = params.pt;
-		float D = (0.5f) / (pt[2] * x + pt[5] * y + pt[8] * z + pt[11]); // 0.5
+		double[] pt = params.pt;
+		double D = (0.5f) / (pt[2] * x + pt[5] * y + pt[8] * z + pt[11]); // 0.5
 																			// ->
 																			// 1.0
 		u[0] = (pt[0] * x + pt[3] * y + pt[6] * z + pt[9]) * D + (0.5f);
 		v[0] = (pt[1] * x + pt[4] * y + pt[7] * z + pt[10]) * D + (0.5f);
 	}
 
-	public void project(float x, float y, float z, float[] u, float[] v,
-			float[] d) {
-		float xc = x / z;
-		float yc = y / z;
+	public void project(double x, double y, double z, double[] u, double[] v,
+			double[] d) {
+		double xc = x / z;
+		double yc = y / z;
 
-		float r2 = xc * xc + yc * yc;
-		float r2d = ((1.0f) + params.distc[0] * r2 + params.distc[1] * r2 * r2 + params.distc[4]
+		double r2 = xc * xc + yc * yc;
+		double r2d = ((1.0f) + params.distc[0] * r2 + params.distc[1] * r2 * r2 + params.distc[4]
 				* r2 * r2 * r2);
-		float xcd = xc * r2d + (2.0f) * params.distc[2] * xc * yc
+		double xcd = xc * r2d + (2.0f) * params.distc[2] * xc * yc
 				+ params.distc[3] * (r2 + (2.0f) * xc * xc);
-		float ycd = yc * r2d + (2.0f) * params.distc[3] * xc * yc
+		double ycd = yc * r2d + (2.0f) * params.distc[3] * xc * yc
 				+ params.distc[2] * (r2 + (2.0f) * yc * yc);
 		xcd = xcd * params.kc[0] + params.kc[6];
 		ycd = ycd * params.kc[4] + params.kc[7];
@@ -181,34 +181,34 @@ public class Calibration {
 	}
 
 	public Point computeUnprojectCoeffs(int _u, int _v) {
-		float u = ((float) _u) / params.width * 2 - 1;
-		float v = ((float) _v) / params.height * 2 - 1;
+		double u = ((double) _u) / params.width * 2 - 1;
+		double v = ((double) _v) / params.height * 2 - 1;
 
 		// Distort camera coordinates
-		float xc = (u - params.kc[6]) / params.kc[0];
-		float yc = (v - params.kc[7]) / params.kc[4];
-		float r2 = xc * xc + yc * yc;
-		float r2c = ((1.0f) + params.invdistc[0] * r2 + params.invdistc[1] * r2
+		double xc = (u - params.kc[6]) / params.kc[0];
+		double yc = (v - params.kc[7]) / params.kc[4];
+		double r2 = xc * xc + yc * yc;
+		double r2c = ((1.0f) + params.invdistc[0] * r2 + params.invdistc[1] * r2
 				* r2 + params.invdistc[4] * r2 * r2 * r2);
-		float xcd = xc * r2c + (2.0f) * params.invdistc[2] * xc * yc
+		double xcd = xc * r2c + (2.0f) * params.invdistc[2] * xc * yc
 				+ params.invdistc[3] * (r2 + (2.0f) * xc * xc);
-		float ycd = yc * r2c + (2.0f) * params.invdistc[3] * xc * yc
+		double ycd = yc * r2c + (2.0f) * params.invdistc[3] * xc * yc
 				+ params.invdistc[2] * (r2 + (2.0f) * yc * yc);
 		xcd = xcd * params.kc[0] + params.kc[6];
 		ycd = ycd * params.kc[4] + params.kc[7];
 
 		// Unnormalized camera rays
-		float dx = params.kc[4] * xcd - params.kc[4] * params.kc[6];
-		float dy = params.kc[0] * ycd - params.kc[0] * params.kc[7];
-		float dz = params.kc[0] * params.kc[4];
+		double dx = params.kc[4] * xcd - params.kc[4] * params.kc[6];
+		double dy = params.kc[0] * ycd - params.kc[0] * params.kc[7];
+		double dz = params.kc[0] * params.kc[4];
 
-		float x = dx / dz;
-		float y = dy / dz;
+		double x = dx / dz;
+		double y = dy / dz;
 		return new Point((int) x, (int) y);
 	}
 
-	public void unproject(int u, int v, int _d, float[] x, float[] y, float[] z) {
-		float d = ((float) _d) / (65535) * params.zmax;
+	public void unproject(int u, int v, int _d, double[] x, double[] y, double[] z) {
+		double d = ((double) _d) / (65535) * params.zmax;
 		Point p = m_unprojCoeffs[v * m_zWidth + u];
 
 		x[0] = p.x * d;
--- ivcam.demo/src/com/example/ivcam/demo/Grey3DView.java
+++ ivcam.demo/src/com/example/ivcam/demo/Grey3DView.java
@@ -1,22 +1,86 @@
 package com.example.ivcam.demo;
 
-import android.os.Bundle;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class Grey3DView extends Activity {
-
+public class Grey3DView extends Activity implements OnClickListener {
+	private Grey3DOpenGLView mGLSurfaceView;
+	private Grey3DOpenGlRender mRenderer;
+	private Button mPreviewButton;
+	private final static int mImageWdith=640;
+	private final static int mImageHeight=480;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_grey3d_view);
-	}
+		mGLSurfaceView = (Grey3DOpenGLView) findViewById(R.id.gl_surface_view);
+		mGLSurfaceView.setEGLContextClientVersion(2);
+		final DisplayMetrics displayMetrics = new DisplayMetrics();
+		getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+		float[] calibData = new float[Calibration.nParamters()];
+		readCalibration(calibData);
+		int[] depth = new int[mImageWdith*mImageHeight];
+		readDepthData(depth);
+		mRenderer = new Grey3DOpenGlRender(calibData, depth, mImageWdith, mImageHeight);
+		mGLSurfaceView.setRenderer(mRenderer, displayMetrics.density);
+		mPreviewButton = (Button) findViewById(R.id.preview_button);
+		mPreviewButton.setOnClickListener(this);
+		}
 
+	private void readCalibration(float[] calibData) {
+		InputStream stream = getResources().openRawResource(R.raw.calibration_params);
+		DataInputStream dataStream = new DataInputStream(stream);
+		for (int i = 0; i<Calibration.nParamters();i++) {
+			try {
+				calibData[i] = (float)dataStream.readFloat();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private void readDepthData(int[] depth) {
+		InputStream stream = getResources().openRawResource(R.raw.hand0_0000);
+		int dataSize = mImageWdith*mImageHeight*2;
+		byte[] rawData = new byte[dataSize];
+		try {
+			stream.read(rawData, 0, dataSize);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		int j = 0;
+		int i = 0;
+		for (; i < dataSize;) {
+			depth[j++] = rawData[i] + (rawData[i + 1] << 8);
+			i += 2;
+		}
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.grey3_dview, menu);
+
 		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.preview_button:
+			finish();
+			break;
+		}		
 	}
 
 }
